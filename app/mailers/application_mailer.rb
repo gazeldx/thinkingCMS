dc899162cb1605ebc94bd88384b46743f449734d
Lane Zhang <mail_zlj@163.com>
2019-01-13 19:43:27 +0800

    Add rubocop and fix all rubocop warnings

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,4 @@
+# Send email
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -2,7 +2,8 @@
 
 # Define an application-wide content security policy
 # For further information see the following documentation
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/
+# Content-Security-Policy
 
 # Rails.application.config.content_security_policy do |policy|
 #   policy.default_src :self, :https
@@ -17,9 +18,11 @@
 # end
 
 # If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+# Rails.application.config.content_security_policy_nonce_generator
+#   = -> request { SecureRandom.base64(16) }
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/
+# Content-Security-Policy-Report-Only
 # Rails.application.config.content_security_policy_report_only = true
